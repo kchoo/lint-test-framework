60df732d817ddf64c47fd8cae6684562da5170f8
kchoo <kchoodev@gmail.com>
2018-02-07 19:18:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
 const {watch} = require('chokidar');
 const {execSync, spawn} = require('child_process');
 const minimatch = require('minimatch');
+const readline = require('readline');
 
-const watcher = watch('**/*.js', {
-	ignored: [
-		'node_modules',
-		'template.test.js'
-	]
-});
+function main () {
+	const watcher = watch('**/*.js', {ignored: ['node_modules', 'template.test.js']});
 
-watcher.
-	on('ready', function () {
-		// lint once we run this script, to avoid making a dummy change to start linting
-		watcher.emit('change');
+	watcher.
+		on('ready', function watcherOnReady () {
+			// lint once we run this script, to avoid making a dummy change to start linting
+			watcher.emit('change');
 
-		watcher.
-			on('add', function (path) {
-				if (!/\.test\.js$/.test(path)) {
-					createNewTestFile(path);
-				}
-			});
-	}).
-	on('change', function () {
-		// clear the console, so that the only thing that is displayed is any errors from this round of linting (if any)
-		process.stdout.write('\x1B[2J\x1B[0f');
+			watcher.
+				on('add', function watcherOnAdd (path) {
+					if (!/\.test\.js$/.test(path)) {
+						createNewTestFile(path);
+					}
+				});
+		}).
+		on('change', function watcherOnChange () {
+			/* clear the console, so that the only thing that is
+			   displayed is any errors from this round of linting (if any) */
+			process.stdout.write('\x1B[2J\x1B[0f');
 
-		const allFilesWatched = getFilesWatched(watcher);
+			const allFilesWatched = getFilesWatched(watcher);
 
-		const lintCommand = `./node_modules/eslint/bin/eslint.js --fix ${allFilesWatched.join(' ')}`;
+			const lintCommand = `./node_modules/eslint/bin/eslint.js --fix ${allFilesWatched.join(' ')}`;
 
-		// pass .test.js files to nodeunit
-		const testFiles = allFilesWatched.
-			filter(
-				minimatch.filter('**/*.test.js')
-			);
+			// pass .test.js files to nodeunit
+			const testFiles = allFilesWatched.
+				filter(minimatch.filter('**/*.test.js'));
 
-		const testCommand = `./node_modules/nodeunit/bin/nodeunit ${testFiles.join(' ')}`;
+			const testCommand = `./node_modules/nodeunit/bin/nodeunit ${testFiles.join(' ')}`;
 
-		try {
-			sh(lintCommand);
-			// run tests iff the linting throws no errors
-			sh(testCommand);
-		} catch (e) {
-			// error is thrown when linting fails, we just want to continue
-		}
-	});
+			try {
+				sh(lintCommand);
+				// run tests iff the linting throws no errors
+				sh(testCommand);
+			} catch (e) {
+				// error is thrown when linting fails, we just want to continue
+			}
+		});
 
-function getFilesWatched(watcher) {
+	readline.
+		createInterface({
+			input: process.stdin,
+			terminal: false
+		}).
+		on('line', function restartScript (line) {
+			if (line === '') {
+				spawn(
+					process.argv[0],
+					process.argv.slice(1),
+					{
+						cwd: process.cwd(),
+						detached: false,
+						stdio: 'inherit'
+					}
+				);
+
+				process.exit();
+			}
+		});
+}
+
+main();
+
+function getFilesWatched (watcher) {
 	const filesArray = [];
 
 	const filesMap = watcher.getWatched();
@@ -54,7 +74,7 @@ function getFilesWatched(watcher) {
 	/* eslint-disable guard-for-in */
 	for (const dir in filesMap) {
 		for (const file of filesMap[dir]) {
-			filesArray.push(dir + '/' + file);
+			filesArray.push(`${dir}/${file}`);
 		}
 	}
 	/* eslint-enable guard-for-in */
@@ -62,38 +82,13 @@ function getFilesWatched(watcher) {
 	return filesArray;
 }
 
-function sh(command) {
+function sh (command) {
 	return execSync(
 		command,
-		{
-			stdio: 'inherit'
-		}
+		{stdio: 'inherit'}
 	);
 }
 
-function createNewTestFile(path) {
+function createNewTestFile (path) {
 	sh(`cp template.test.js ${path.replace('.js', '.test.js')}`);
 }
-
-const readline = require('readline');
-
-readline.
-	createInterface({
-		input: process.stdin,
-		terminal: false
-	}).
-	on('line', function restartScript(line) {
-		if (line === '') {
-			spawn(
-				process.argv[0],
-				process.argv.slice(1),
-				{
-					cwd: process.cwd(),
-					detached: false,
-					stdio: 'inherit'
-				}
-			);
-
-			process.exit();
-		}
-	});
