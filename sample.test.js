c8298b82a43ae6d8494b44f2a8e03236fa168efe
kchoo <kchoodev@gmail.com>
2018-02-11 21:53:41 -0600

    Update sample.test.js

--- sample.test.js
+++ sample.test.js
@@ -1,14 +1,24 @@
+/**
+ * sample function, turns strings into floats
+ * @type {function}
+ */
 const sample = require('./sample');
 
 module.exports = {
-	happy: function (test) {
-		const random = Math.random();
-		const numberAsString = String(random);
-
-		const number = sample(numberAsString);
-
-		test.strictEqual(number, random);
-
-		test.done();
-	}
+	happy
 };
+
+/**
+ * @param  {object} test - NodeUnit test object
+ * @return {void}
+ */
+function happy(test) {
+	const random = Math.random();
+	const numberAsString = String(random);
+
+	const number = sample(numberAsString);
+
+	test.strictEqual(number, random);
+
+	test.done();
+}
